f337253a8a3685b706c8e26e3528922c98419c14
Minerva <superetro85@gmail.com>
2025-06-15 22:53:49 -0300

    undestanding async js

---
+++ 01-node-tutorial/async-js/1.js
@@ -0,0 +1,11 @@
+const name = "Wellington Minerva"; //  Declara uma string chamada nome
+
+
+
+
+const makeGreeting = (name) => {
+    return `Hello, my name is ${name}`
+}
+
+const greeting = makeGreeting(name); // Declara outra ...
+console.log(greeting)
